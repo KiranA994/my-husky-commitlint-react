2f23535da0e1da6ac268cba29e9d67962d08ae5b
KiranA994 <kiranagrasala13@gmail.com>
2025-05-10 13:26:03 +0530

    fix: Eslint package updated

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -20,13 +20,11 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    "extends": ["react-app", "eslint:recommended", "prettier"],
-  "rules": {
-    "semi": ["error", "never"]
-  },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      semi: ['error', 'never'],
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
@@ -34,4 +32,4 @@ export default [
       ],
     },
   },
-]
+];
